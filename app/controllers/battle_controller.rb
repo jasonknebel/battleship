68ed9d3d99c90ce05eea2a97218f62d222df427a
Jason Knebel <jasonkneb@gmail.com>
2013-05-29 18:12:39 -0400

    adding register

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -3,7 +3,9 @@ class BattleController < ApplicationController
   require 'json'
 
   def play
-    session[:game_id] = 2154
+    if !session[:game_id] then 
+      redirect_to root_path, :alert => 'no game id'
+    end
   end
 
   def nuke
@@ -19,5 +21,26 @@ class BattleController < ApplicationController
 
     render json: response
   end
+
+
+  def register
+
+    reset_session
+    request = {name: params[:name], email: params[:email]}
+
+    begin
+      # response = self.class.post('http://battle.platform45.com/register',
+      #   body: request.to_json)
+      # response['success'] = true
+      response = {"id"=>"2154", "x"=>3, "y"=>4, "success"=>true}
+      session[:game_id] = response['id']
+    rescue
+      response = {success: false, message: 'problem contacting platform45'}
+    end
+
+    render json: {:csrfToken => form_authenticity_token}.merge(response)
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Battleship::Application.routes.draw do
 
-  root to: 'battle#play'
+  root to: 'battle#home'
+  get '/play' => 'battle#play'
 
   post '/nuke' => 'battle#nuke'
+  post '/register' => 'battle#register'
 
 end
